424ab8ee162708c9d0a05db193a141268855801a
Gogo Vasev <gvasevg@gmail.com>
2022-07-11 10:03:39 +0300

    initial commit.

---
+++ demoRestTest-1/src/main/java/com/vasev/demoRestTest/DemoRestTestApplication.java
@@ -0,0 +1,13 @@
+package com.vasev.demoRestTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestTestApplication.class, args);
+	}
+
+}
---
+++ demoRestTest-1/src/test/java/com/vasev/demoRestTest/DemoRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasev.demoRestTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
